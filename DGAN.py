ca5fdb06aeb9e6cd2fd859f5adbb4ffa1f572894
Ainaz Jamshidi <47214448+ajam74001@users.noreply.github.com>
2023-10-19 20:31:02 -0400

    Update DGAN.py

--- DGAN.py
+++ DGAN.py
@@ -6,7 +6,6 @@ from gretel_synthetics.timeseries_dgan.dgan import DGAN
 from gretel_synthetics.timeseries_dgan.config import DGANConfig, OutputType
 
 #TODO: add maybe the attributes as well (demographics) - 
-
 # %%capture
 data = pd.read_csv("/userfiles/ajamshidi18/PCG_timeseris_normal.csv")
 print(data.isna().sum().sum())
@@ -14,7 +13,8 @@ print(data.isna().sum().sum())
 features = np.array(data).reshape(data.shape[0], data.shape[1], 1)
 
 # Train DGAN model
-#TODO: I have no idea what is sample_len :time series steps to generate from each LSTM cell in DGAN
+# sample_len :time series steps to generate from each LSTM cell in DGAN
+# TODO: loging of the performance 
 model = DGAN(DGANConfig(
     max_sequence_len=features.shape[1],
     sample_len=1489,
